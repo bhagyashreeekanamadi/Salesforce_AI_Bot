99952c3065791e535eeb3562102f6cc294ae3b93
Bhagyashree Kanamadi <82951348+bhagyashreeekanamadi@users.noreply.github.com>
2026-05-16 18:39:43 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -12,7 +12,9 @@ st.set_page_config(
     layout="centered"
 )
 
-BACKEND_URL = "http://127.0.0.1:8000"
+# BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://salesforce-interview-ai-backend.onrender.com"
+
 
 # ==========================================
 # TITLE
